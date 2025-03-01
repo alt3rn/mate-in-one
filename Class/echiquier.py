cc7e284dfcb09b7ddadd41ad8648f477bf83333c
bobea <arthur.bobe@etu.univ-grenoble-alpes.fr>
2025-03-01 23:22:49 +0100

    Affichage board, modification du board suite à un mouvement. Explications en détails dans le script

--- Class/echiquier.py
+++ Class/echiquier.py
@@ -1,12 +1,20 @@
 COLUMNS = ["a","b","c","d","e","f","g","h"]
-CONVERT_COL= {'a': 1,
-               'b': 2,
-               'c': 3,
-               'd': 4,
-               'e': 5,
-               'f': 6,
-               'g': 7,
-               'h': 8
+CONVERT = {'a': 0,
+               'b': 1,
+               'c': 2,
+               'd': 3,
+               'e': 4,
+               'f': 5,
+               'g': 6,
+               'h': 7,
+               0: 'a',
+               1: 'b',
+               2: 'c',
+               3: 'd',
+               4: 'e',
+               5: 'f',
+               6: 'g',
+               7: 'h'
                }
 
 class Board:
@@ -36,24 +44,35 @@ class Board:
         """
         echiquier = []
         
-        for i in range(1, 9):
+        for i in range(0, 8):
             ligne = []
-            for j in range(1, 9):
+            for j in range(0, 8):
                 ligne.append('_')
             echiquier.append(ligne)
         return echiquier
     
 
-    def convertPosition(position: str):
+    def convertIndice(position: str):
         """
         Renvoie les indices utilisables pour les listes construisant le plateau
         position: str - Position du pion. Ex: 'e4', 'b8'
         ---
         return: int, int
         """
-        line = CONVERT_COL[position[0]]
-        col = int(position[2])
+        col = CONVERT[position[0]]
+        line = int(position[1])-1
         return line, col
+    
+    def convertPosition(col: int, line: int):
+        """
+        La fonction convertIndice() mais en reverse : donne un équivalent d'indices pour une position donnée.
+        col: int - Numéro de la colonne
+        line: int - Numéro de la ligne (-1 par rapport à la ligne réelle comme c'est un indice)
+        ---
+        return: str - La position équivalente
+        """
+        column = CONVERT[col]
+        return f"{column}{line+1}"
 
 
     def movement(board: list, piece: str, next_position: str):
@@ -65,10 +84,22 @@ class Board:
         ---
         return: list - Plateau avec le mouvement réalisé. L'ancienne position du pion après le mouvement est ainsi vide.
         """
-        for line in board:
-            position_actuelle = line.find(piece)
-        col_next, line_next = Board.convertPosition(next_position)
-        board[col_next][line_next] = piece
-        col_old, line_old = Board.convertPosition(position_actuelle)
-        board[col_old][line_old] = '_'
+        for i in range(0, 8):
+            if piece in board[i]:  # Implique que chaque pièce a un nom UNIQUE
+                actual_position_col = board[i].index(piece)
+                actual_position_line = i
+                actual_position = Board.convertPosition(actual_position_col, actual_position_line)
+        line_next, col_next = Board.convertIndice(next_position)
+        board[line_next][col_next] = piece
+        line_old, col_old = Board.convertIndice(actual_position)
+        board[line_old][col_old] = '_'
         return board
+    
+
+    def display(board):
+        for i in range(len(board)-1, -1, -1):
+            ligne_print = ''
+            for square in board[i]:
+                ligne_print += f"{square} "
+            print(ligne_print)
+        return None
--- main.py
+++ main.py
@@ -4,8 +4,26 @@ import pprint
 
 Piece1 = Piece()
 board = Board.initBoardDico()
-board2 = Board.initBoardList()
+board_test = Board.initBoardList()
 Piece1.pion()
 
-pprint.pprint(board)
-pprint.pprint(board2)
+
+def main():
+    # Les indices dans le board sont inversés : a3 correspond à [2][0], soit 3a
+
+    # Cette partie permet de simuler un coup pion e4 (pour les blancs). Pour un print correct
+    # du jeu avec les blancs en bas et le plateau dans le bon sens, il faut parcourir avec un for i in range(.., ..., -1)
+    board_test[1][4] = 'p'
+    print('Avant le mouvement')
+    pprint.pprint(board_test)
+    print(f" ----- ")
+    Board.movement(board_test, 'p', 'e4') 
+    print('Après le mouvement :')
+    pprint.pprint(board_test)
+
+    Board.display(board_test)  # Première idée de ce à quoi ressemble le board_test, dans le bon sens.
+
+    pass
+
+
+main()
