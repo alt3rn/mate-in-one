e80d0e08aebe224c78c96a74800dcb8af415d8a4
bobea <arthur.bobe@etu.univ-grenoble-alpes.fr>
2025-03-02 20:42:23 +0100

    minor changes sur la classe Pion

--- Class/pieces.py
+++ Class/pieces.py
@@ -1,9 +1,31 @@
 from Class.echiquier import *
+CONVERT = {'a': 0,
+               'b': 1,
+               'c': 2,
+               'd': 3,
+               'e': 4,
+               'f': 5,
+               'g': 6,
+               'h': 7,
+               0: 'a',
+               1: 'b',
+               2: 'c',
+               3: 'd',
+               4: 'e',
+               5: 'f',
+               6: 'g',
+               7: 'h'
+               }
 
-class Piece:
-    def __init__(self):
-        self.position = Board
+class Pawn:
+    def __init__(self, numero, color):
+        self.position = f'{CONVERT[numero-1]}2'
+        if color == 'white':
+            self.name = f'❖P{numero}'
+        elif color == 'black':
+            self.name = f'◇P{numero}'
+    
 
-    def pion(self):
-        print("Pion")
+    
+        
 
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from Class.pieces import *
 from Class.echiquier import *
 import pprint
 
-Piece1 = Piece()
+
 board_test = Board.initBoard()
-Piece1.pion()
+pawn1 = Pawn(1, 'white')
+print(pawn1.positionInit)
 
 
 SLOTS = []
@@ -14,7 +15,7 @@ for column in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
 
     
 
-def player_turn(slots_available, pieces_available):
+def player_turn_white(slots_available, pieces_available):
     piece_to_move = input('Quelle pièce souhaitez-vous jouer ? ')
     while piece_to_move not in pieces_available:
         piece_to_move = input('Quelle pièce souhaitez-vous jouer ? ')
@@ -30,11 +31,8 @@ def player_turn(slots_available, pieces_available):
 def main():
     # Les indices dans le board sont inversés : a3 correspond à [2][0], soit 3a
 
-    # Cette partie permet de simuler un coup pion e4 (pour les blancs). Pour un print correct
-    # du jeu avec les blancs en bas et le plateau dans le bon sens, il faut parcourir avec un for i in range(.., ..., -1)
     # board_test[1][4] = '❖P5'
-    Board.display(board_test) 
-    # piece_to_move = input('Quelle pièce souhaitez-vous jouer ? ')
+    #     # piece_to_move = input('Quelle pièce souhaitez-vous jouer ? ')
     # where_to_move = input('Où voulez-vous jouer cette pièce ? ')
     # Board.movement(board_test, piece_to_move, where_to_move)
     Board.display(board_test)  # Première idée de ce à quoi ressemble le board_test, dans le bon sens.
@@ -45,7 +43,7 @@ def main():
     game_ended = False
     while not game_ended:
         moves += 1
-        piece_to_move, where_to_move = player_turn(SLOTS, ['❖P1', '❖P3', "❖P5", "❖P7", "❖Q"])
+        piece_to_move, where_to_move = player_turn_white(SLOTS, ['❖P1', '❖P3', "❖P5", "❖P7", "❖Q"])
         Board.movement(board_test, piece_to_move, where_to_move)
         Board.display(board_test)
         if moves == 5:
